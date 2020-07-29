1eb3b2ed78f43adc2dd9641cf43fd660670a88be
senocax <facebookshop@hotmail.com>
2020-07-29 17:26:28 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
+import Title from './components/Title';
+import {SearchForm} from './components/SearchForm'
+import { render } from '@testing-library/react';
 
-function App() {
+import {Detail} from './pages/Detail'
+import {MoviesList} from './components/MovieList'
+
+class App extends Component {
+  state ={ usedSearch:false, results : []}
+
+  _handleResults= (results) => {
+    this.setState ({results, usedSearch: true})
+  }
+  _renderResults () {
+    return this.state.results.length === 0
+      ? <p>Sin resultado</p>
+      : <MoviesList movies ={this.state.results} />
+  }
+ render () {
+  const url = new URL (document.location)
+  const hasID = url.searchParams.has ('id')
+
+  if (hasID) {
+   return <Detail id= {url.searchParams.get('id')}/>
+  }
+
   return (
+
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title> search movies </Title>
+      <div className='SearchForm-wrapper'>
+         <SearchForm onResults= {this._handleResults}/>
+      </div>
+      {this.state.usedSearch 
+        ? this._renderResults ()
+        : <small> Use the form to seach a movie</small>}
+  
     </div>
   );
 }
+}
+
 
 export default App;
